7eb6bc0181b8d8eef830cd4942de92a0cd6bc59c
vrajdesai78 <vrajdesai78@gmail.com>
2023-06-19 18:22:19 +0530

    Fixed audio issue

--- src/components/Peers.tsx
+++ src/components/Peers.tsx
@@ -1,14 +1,14 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
   BiCamera,
   BiCameraOff,
   BiMicrophone,
   BiMicrophoneOff,
-} from 'react-icons/bi';
+} from "react-icons/bi";
 
-import { Button, Center, Icon } from '@chakra-ui/react';
-import { Audio, Video } from '@huddle01/react/components';
-import { useAudio, usePeers, useVideo } from '@huddle01/react/hooks';
+import { Button, Center, Icon } from "@chakra-ui/react";
+import { Audio, Video } from "@huddle01/react/components";
+import { useAudio, usePeers, useVideo } from "@huddle01/react/hooks";
 
 export default function Peers() {
   const { peers } = usePeers();
@@ -19,12 +19,22 @@ export default function Peers() {
   return (
     <div className="flex flex-col items-center justify-center max-w-6xl mx-auto">
       <Center className="grid grid-cols-4 gap-4 mt-5">
-        {Object.values(peers).map(peer => {
+        {Object.values(peers).map((peer) => {
           if (peer.cam) {
             return (
               <div className="w-80" key={peer.peerId}>
                 <Video peerId={peer.peerId} track={peer.cam} />
               </div>
+            );
+          }
+          if (!peer.cam) {
+            return (
+              <Center
+                className="w-80 h-60 bg-black items-center justify-center"
+                key={peer.peerId}
+              >
+                {peer.peerId}
+              </Center>
             );
           }
           if (peer.mic) {
@@ -34,19 +44,15 @@ export default function Peers() {
               </div>
             );
           }
-          if (!peer.cam) {
-            return (
-              <Center className="w-80 h-60 bg-black items-center justify-center" key={peer.peerId}>
-                {peer.peerId}
-              </Center>
-            );
-          }
         })}
       </Center>
 
       <Center className="grid grid-cols-4 gap-4 mt-5 items-center justify-center">
-        {Object.values(peers).map(peer => (
-          <div key={peer.peerId} className="w-80 flex items-center justify-center">
+        {Object.values(peers).map((peer) => (
+          <div
+            key={peer.peerId}
+            className="w-80 flex items-center justify-center"
+          >
             {peer.cam?.enabled ? (
               <Button
                 mr={2}
@@ -60,7 +66,7 @@ export default function Peers() {
             ) : (
               <Button
                 mr={2}
-                background={'red.600'}
+                background={"red.600"}
                 className="flex items-center justify-center"
                 onClick={() => {
                   createCamConsumer(peer.peerId);
@@ -79,10 +85,10 @@ export default function Peers() {
               >
                 <Icon as={BiMicrophone} />
               </Button>
-            ) : ( 
+            ) : (
               <Button
                 mr={2}
-                background={'red.600'}
+                background={"red.600"}
                 className="flex items-center justify-center"
                 onClick={() => {
                   createMicConsumer(peer.peerId);
